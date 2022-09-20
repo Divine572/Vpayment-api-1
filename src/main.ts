8c3660f36bf0eb771d4c6ccd3f2e0aa71f91e9cd
divicodes <divinef837@gmail.com>
2022-09-20 15:28:35 +0100

    added  joi to validate env variables

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.use(cookieParser());
+  app.setGlobalPrefix('v1/api');
   await app.listen(3000);
 }
 bootstrap();
